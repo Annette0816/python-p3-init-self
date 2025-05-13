5e16ec287a58c3dd67c7d9284cf3ad1fab0bd083
Annette <annette.ngugi@student.moringaschool.com>
2025-05-13 11:31:51 +0300

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__ (self, name, breed="Mutt",favorite_toy="Any"):
+        self.name = name
+        self.favorite_toy= favorite_toy
+        self.breed = breed
+        
+
+    def bark(self):
+        print("Woof!")
+
+    
+fido = Dog("Fido","Dalmatian")
+fido.favorite_toy
+
+
+fido.owner = "Sophie"
+
+
+
+snoopy = Dog("Snoopy","Tennis Ball")
+snoopy.favorite_toy
+   
